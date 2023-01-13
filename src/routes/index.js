289b9a66185fbe56fd5c70184e8f3016d7239ccc
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-01-13 15:54:28 -0500

    uptade index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/home", (req, res) => {
   res.render("index", { title: "Inicio de Node" });
 });
 
