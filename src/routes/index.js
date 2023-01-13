4623a6cc3b3751df8b28ff7060974e98cdd866a5
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-01-13 17:32:30 -0500

    Se actualizo las rutas y la estructura

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/home", (req, res) => {
-  res.render("index", { title: "Inicio de Node" });
+  res.render("index", { title: "Inicio de pagina con node.js" });
 });
 
 router.get("/about", (req, res) => {
-  res.render("about", { title: "Sobre.." });
+  res.render("about", { title: 'Esta es una plantilla de un "sitio web"' });
 });
 router.get("/Contact", (req, res) => {
-  res.render("contact", { title: "Contacto" });
+  res.render("contact", { title: "Más Información del Proyecto" });
 });
 
 export default router;
